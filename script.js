2fe88dfa4e45b7bdf3dc8b2b7dad6a8c394c7226
Pascal Congès <pascal@aufondagauche.fr>
2016-03-02 00:30:04 +0000

    Multiple selectors, enlarged viewport

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-var casper = require('casper').create();
+var casper = require('casper').create({viewportSize: {width: 800, height: 600}});
 // console.log("coucuo");
 // var casper = require("casper");
 
 casper.start('http://localhost:3000', function() {
-    this.captureSelector('grab.png', '.selector');
+    this.captureSelector('grab1.png', '#s1');
+    this.captureSelector('grab2.png', '#s2');
 	console.log("done");
 });
 
