6c0b4a69da8cbe757e028132c2baadeb85996beb
Pascal Congès <pascal@aufondagauche.fr>
2016-03-02 00:33:47 +0000

    Ignore test

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ var casper = require('casper').create({viewportSize: {width: 800, height: 600}})
 // var casper = require("casper");
 
 casper.start('http://localhost:3000', function() {
-    this.captureSelector('grab1.png', '#s1');
-    this.captureSelector('grab2.png', '#s2');
+    this.captureSelector('output/grab1.png', '#s1');
+    this.captureSelector('output/grab2.png', '#s2');
 	console.log("done");
 });
 
